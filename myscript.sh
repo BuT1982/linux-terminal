c82d36a3fdd6f55b62c5576775296969a5e29ba1
BuT1982 <but1982@mail.ru>
2021-05-31 21:14:25 +0300

    add script.sh

---
+++ myscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir folder
+cd folder
+mkdir folder1 folder2 folder3
+cd folder2
+touch {1,2,3}.txt {4,5,6}.json
+mkdir folder4 folder5 folder6
+ls -la
+mv 4.json 2.txt  ~/Documents/VadimKsendzov/linux-terminal/folder
